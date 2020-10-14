b76273cff013118b2263575408ab6b5174e3ff0c
M Fadlul Hafiizh <muh.hafiizh56@gmail.com>
2020-10-15 06:48:10 +0700

    shared animation bug

--- app/src/main/java/com/application/twksupport/auth/SplashActivity.java
+++ app/src/main/java/com/application/twksupport/auth/SplashActivity.java
@@ -1,10 +1,21 @@
 package com.application.twksupport.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.application.twksupport.R;
 import com.application.twksupport.model.TokenResponse;
@@ -12,24 +23,39 @@ import com.application.twksupport.UserActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private ConstraintLayout parentLayout;
+    ImageView twkLogo;
+    TextView txtSupport;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        twkLogo = findViewById(R.id.twklogo);
+        txtSupport = findViewById(R.id.twkSupport);
+        parentLayout = findViewById(R.id.splash_layout);
+
         Thread thread = new Thread(){
             @Override
             public void run() {
                 try {
-                    sleep(3500);
-                    actUserLogin();
+                    sleep(3000);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            actUserLogin();
+                        }
+                    });
                     finish();
                 }
                 catch (Exception e){
                     e.printStackTrace();
+                    Log.d("machine", e.getMessage());
                 }
             }
         };
         thread.start();
+
     }
 
     private void actUserLogin(){
@@ -40,8 +66,12 @@ public class SplashActivity extends AppCompatActivity {
             startActivity(toUser);
         }
         else {
-            Intent login = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(login);
+            Intent login = new Intent(SplashActivity.this, MainActivity.class);
+            Pair[] pairs = new Pair[2];
+            pairs[0] = new Pair<View, String>(twkLogo, "sendLogo");
+            pairs[1] = new Pair<View, String>(txtSupport, "sendTitle");
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SplashActivity.this, pairs);
+            startActivity(login, options.toBundle());
         }
     }
 }
