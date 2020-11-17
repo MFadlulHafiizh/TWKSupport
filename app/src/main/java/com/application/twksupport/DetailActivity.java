6c9a5b7139cb050111cb0ded872a1101370ab841
M Fadlul Hafiizh <muh.hafiizh56@gmail.com>
2020-11-17 15:43:19 +0700

    admin input deadline validation

--- app/src/main/java/com/application/twksupport/DetailActivity.java
+++ app/src/main/java/com/application/twksupport/DetailActivity.java
@@ -21,6 +21,7 @@ import android.widget.TextView;
 import com.application.twksupport.RestApi.ApiClient;
 import com.application.twksupport.RestApi.ApiService;
 import com.application.twksupport.adapter.RvStaffListAdapter;
+import com.application.twksupport.auth.MainActivity;
 import com.application.twksupport.model.BugsData;
 import com.application.twksupport.model.FeatureData;
 import com.application.twksupport.model.StaffResponse;
@@ -34,6 +35,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -67,25 +69,34 @@ public class DetailActivity extends AppCompatActivity {
         sendId = bugsData.getId_ticket();
         status = bugsData.getStatus();
         datePicker(etyear, etmonth, etday);
-        if (status.equals("Reported")){
+        if (status.equals("Reported")) {
             btnAssign.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    String datePick = etyear.getText().toString()+"-"+etmonth.getText().toString()+"-"+etday.getText().toString();
-                    Intent toStafflist = new Intent(DetailActivity.this, StaffListActivity.class);
-                    toStafflist.putExtra(StaffListActivity.EXTRA_TICKET, bugsData);
-                    toStafflist.putExtra(StaffListActivity.EXTRA_DATE, datePick);
-                    startActivity(toStafflist);
+                    final String datePick = etyear.getText().toString() + "-" + etmonth.getText().toString() + "-" + etday.getText().toString();
+                    if (!datePick.equals("--")){
+                        Intent toStafflist = new Intent(DetailActivity.this, StaffListActivity.class);
+                        toStafflist.putExtra(StaffListActivity.EXTRA_TICKET, bugsData);
+                        toStafflist.putExtra(StaffListActivity.EXTRA_DATE, datePick);
+                        startActivity(toStafflist);
+                    }
+                    else{
+                        new SweetAlertDialog(DetailActivity.this, SweetAlertDialog.ERROR_TYPE)
+                                .setTitleText("Please input the deadline")
+                                .setConfirmText("Ok")
+                                .setConfirmButtonBackgroundColor(Color.parseColor("#FFFF9800"))
+                                .show();
+                    }
                 }
             });
         }
-        else{
+        else {
             container.setVisibility(View.INVISIBLE);
         }
 
     }
 
-    private void datePicker(final EditText dateYear, EditText dateMonth, EditText dateDay){
+    private void datePicker(final EditText dateYear, EditText dateMonth, EditText dateDay) {
         Calendar calendar = Calendar.getInstance();
         final int year = calendar.get(Calendar.YEAR);
         final int month = calendar.get(Calendar.MONTH);
@@ -96,7 +107,7 @@ public class DetailActivity extends AppCompatActivity {
             public void onClick(View view) {
                 DatePickerDialog datePickerDialog = new DatePickerDialog(
                         DetailActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth
-                ,setListener, year, month, day);
+                        , setListener, year, month, day);
                 datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 datePickerDialog.show();
             }
@@ -105,10 +116,10 @@ public class DetailActivity extends AppCompatActivity {
         setListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
-                month = month+1;
-                String tahun = ""+year;
-                String bulan = ""+month;
-                String hari = ""+dayOfMonth;
+                month = month + 1;
+                String tahun = "" + year;
+                String bulan = "" + month;
+                String hari = "" + dayOfMonth;
                 etyear.setText(tahun);
                 etmonth.setText(bulan);
                 etday.setText(hari);
